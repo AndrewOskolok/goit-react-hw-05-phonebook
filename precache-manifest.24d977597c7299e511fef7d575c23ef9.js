60ba610c7e04676273c87c9690fa528c9f43e1b2
Oscolok <oscolok7@gmail.com>
2020-08-02 13:56:53 +0300

    Updates

---
+++ precache-manifest.24d977597c7299e511fef7d575c23ef9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c1e48d764a28f2dd1cad6e8c645b11",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "1a410a9f760d320a2437",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.8f2d1ff1.chunk.css"
+  },
+  {
+    "revision": "6f6561217ba4fd138c7f",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.b3662370.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.b3662370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a410a9f760d320a2437",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.7b709875.chunk.js"
+  },
+  {
+    "revision": "d7cbcecc002359ec1145",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.b3e32233.js"
+  }
+]);
